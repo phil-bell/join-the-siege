66212bf16abaa37c38191e2af241027093580a75
Philip Bell <philhabell@gmail.com>
2024-11-25 20:22:43 +0000

    adding test for classifier

---
+++ tests/test_classifier.py
@@ -0,0 +1,20 @@
+import pytest
+from werkzeug.datastructures import FileStorage
+
+from src.classifier import classify_file
+
+
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        ("drivers_license.pdf", "drivers_licence"),
+        ("bank_statement.png", "bank_statement"),
+        ("invoice.jpg", "invoice"),
+    ],
+)
+def test_classify_file(filename, expected):
+    file = FileStorage(filename=filename)
+
+    response = classify_file(file)
+
+    assert response == expected
